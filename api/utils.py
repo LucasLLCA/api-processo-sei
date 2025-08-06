978126b93ebb29da9c1ea64afa9043052132ea54
Lucas Cortez <lucascortez.llca@gmail.com>
2025-08-06 18:44:35 -0300

    remoçao de funções nao utilizadas

--- api/utils.py
+++ api/utils.py
@@ -4,33 +4,12 @@ import io
 from minio import Minio
 from minio.error import S3Error
 
-def converte_documentos_para_markdown(caminho_arquivo: str):
-    if not os.path.isfile(caminho_arquivo):
-        return
-    with open(caminho_arquivo, "r", encoding="utf-8") as f:
-        conteudo = f.read()
-
-    texto_limpo = re.sub(r'<[^>]+>', '', conteudo)
-    base, _ = os.path.splitext(caminho_arquivo)
-    caminho_md = base + ".md"
-
-    with open(caminho_md, "w", encoding="utf-8") as f:
-        f.write(texto_limpo)
-
-    return caminho_md
-
 def converte_html_para_markdown_memoria(html_content: str) -> str:
     """
     Converte conteúdo HTML para Markdown diretamente em memória
     """
     texto_limpo = re.sub(r'<[^>]+>', '', html_content)
     return texto_limpo
-
-def ler_arquivo_md(caminho_md: str) -> str:
-    if not caminho_md or not os.path.isfile(caminho_md):
-        return ""
-    with open(caminho_md, "r", encoding="utf-8") as f:
-        return f.read()
 
 def ler_conteudo_md(md_data: str) -> str:
     """
