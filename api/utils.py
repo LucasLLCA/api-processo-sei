b13beac4bad3f157d705105fc3d545a11e63045d
Lucas Cortez <lucascortez.llca@gmail.com>
2025-08-08 08:00:51 -0300

    alteraçao secure

--- api/utils.py
+++ api/utils.py
@@ -44,7 +44,8 @@ def ler_arquivo_md_minio(object_name: str) -> str:
         minio_client = Minio(
             settings.MINIO_ENDPOINT,
             access_key=settings.MINIO_ACCESS_KEY,
-            secret_key=settings.MINIO_SECRET_KEY
+            secret_key=settings.MINIO_SECRET_KEY,
+            secure=False
         )
         
         # Baixar o objeto do MinIO
